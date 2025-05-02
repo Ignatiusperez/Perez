b63899936f656a6613d8197aca0fab95d0018d4e
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-05-02 10:09:54 +0300

    Update 𝙋𝙀𝙍𝙀𝙕-𝙈𝘿.js

--- 𝙋𝙀𝙍𝙀𝙕-𝙈𝘿.js
+++ 𝙋𝙀𝙍𝙀𝙕-𝙈𝘿.js
@@ -932,7 +932,7 @@ case 'save': {
       }
 
       // Confirm in original chat
-      await m.reply("✅ Status saved to your DM!");
+      await m.reply("perez saved!");
 
     } catch (error) {
       console.error("Save error:", error);
@@ -941,6 +941,97 @@ case 'save': {
   }
 }
 break;
+const events = process.env.EVENTS || 'false';
+const botname = process.env.BOTNAME || 'PEREZ_MD;
+
+const getContextInfo = (m) => {
+    return {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363266249040649@newsletter',
+            newsletterName: 'perez Support',
+            serverMessageId: 143,
+        },
+    };
+};
+
+const Events = async (client, perez) => {
+    const Myself = await client.decodeJid(client.user.id);
+
+    try {
+        let metadata = await client.groupMetadata(keizzah.id);
+        let participants = keizzah.participants;
+        let desc = metadata.desc || "No Description";
+        let groupMembersCount = metadata.participants.length;
+
+        for (let num of participants) {
+            let dpuser;
+            let userName = num.split("@")[0]; // Extracting sender's name
+
+            try {
+                dpuser = await client.profilePictureUrl(num, "image");
+            } catch {
+                dpuser = "https://i.imgur.com/iEWHnOH.jpeg";
+            }
+
+            // Capture timestamps
+            const timeJoined = new Date().toLocaleString();
+            const timeLeft = new Date().toLocaleString();
+
+            if (keizzah.action === "add") {
+                const WelcomeText = `Hey @${userName} 👋
+Welcome to *${metadata.subject}*.
+You are member number ${groupMembersCount} in this group. 🙏
+Time joined: *${timeJoined}*
+Please read the group description to avoid being removed:
+${desc}
+*Powered by ${botname}.`;
+
+                if (events === 'true') {
+                    await client.sendMessage(keizzah.id, {
+                        image: { url: dpuser },
+                        caption: WelcomeText,
+                        mentions: [num],
+                        contextInfo: getContextInfo({ sender: Myself }),
+                    });
+                }
+            } else if (keizzah.action === "remove") {
+                const GoodbyeText = `Goodbye @${userName}. 😔
+Another member has left the group.
+Time left: *${timeLeft}*
+The group now has ${groupMembersCount} members. 😭`;
+
+                if (events === 'true') {
+                    await client.sendMessage(perez.id, {
+                        image: { url: dpuser },
+                        caption: GoodbyeText,
+                        mentions: [num],
+                        contextInfo: getContextInfo({ sender: Myself }),
+                    });
+                }
+            } else if (keizzah.action === "demote" && events === 'true') {
+                await client.sendMessage(perez.id, {
+                    text: `@${perez.author.split("@")[0]} has demoted @${perez.participants[0].split("@")[0]} from admin. 👀`,
+                    mentions: [keizzah.author, perez.participants[0]],
+                    contextInfo: getContextInfo({ sender: Myself }),
+                });
+            } else if (keizzah.action === "promote" && events === 'true') {
+                await client.sendMessage(perez.id, {
+                    text: `@${perez.author.split("@")[0]} has promoted @${perez.participants[0].split("@")[0]} to admin. 👀`,
+                    mentions: [perez.author, perez .participants[0]],
+                    contextInfo: getContextInfo({ sender: Myself }),
+                });
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = Events;
+
 	
 	      case 'impressive': {
 		      var mumaker = require("mumaker");
