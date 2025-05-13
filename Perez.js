88581797835361171cd728b2db9944adaa24e842
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-05-13 06:15:02 +0300

    Update Perez.js

--- Perez.js
+++ Perez.js
@@ -1949,28 +1949,7 @@ reply(resultt.stderr)
 
 break;
 		      case 'save': {
-  const textL = m.text.toLowerCase();
-  const quotedMessage = m.msg?.contextInfo?.quotedMessage;
-
-if (quotedMessage && textL.startsWith(prefix + "save") && !m.quoted.chat.includes("status@broadcast")) {
-    return m.reply("You did not tag a status media to save.");
-  }
-
-if (Owner && quotedMessage && textL.startsWith(prefix + "save") && m.quoted.chat.includes("status@broadcast")) {
-    
-    if (quotedMessage.imageMessage) {
-      let imageCaption = quotedMessage.imageMessage.caption;
-      let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
-      client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
-    }
-
-    if (quotedMessage.videoMessage) {
-      let videoCaption = quotedMessage.videoMessage.caption;
-      let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
-      client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
-    }
-     }
-      }
+			      
     break;
 	      case 'gitclone': {
 		      if (!text) return m.reply(`Where is the link?`)
@@ -2391,7 +2370,7 @@ case 'save': {
       }
 
       // Confirm in original chat
-      await m.reply("seen");
+      await m.reply("🧚");
 
     } catch (error) {
       console.error("Save error:", error);
