f3fc4ff5ad07cf22bb3b51319f5dd0eb84d6b578
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-07-29 21:45:34 +0300

    Update Perez.js

--- Perez.js
+++ Perez.js
@@ -28,10 +28,6 @@ module.exports = Perez = async (client, m, chatUpdate, store) => {
     var body =
       m.mtype === "conversation"
         ? m.message.conversation
-        : m.mtype == "imageMessage"
-       ? m.message.imageMessage.caption
-        : m.mtype == "videoMessage"
-        ? m.message.videoMessage.caption
         : m.mtype == "extendedTextMessage"
         ? m.message.extendedTextMessage.text
         : m.mtype == "buttonsResponseMessage"
