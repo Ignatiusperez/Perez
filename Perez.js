9be3c6ef8b2241791724f5cd3687e2c84b001a48
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-11-07 22:48:05 +0300

    Update Perez.js

--- Perez.js
+++ Perez.js
@@ -568,6 +568,36 @@ case "gptdm": {
   reply(`✅ Gptdm has been turned *${text.toUpperCase()}*`);
 }
 break;
+			  
+case 'gcprofile': {
+ function convertTimestamp(timestamp) {
+  const d = new Date(timestamp * 1000);
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return {
+    date: d.getDate(),
+    month: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(d),
+    year: d.getFullYear(),
+    day: daysOfWeek[d.getUTCDay()],
+    time: ${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()}
+  }
+}
+
+if (!m.isGroup) return m.reply("This command is meant for groups");
+
+let info = await client.groupMetadata(m.chat);
+let ts = await convertTimestamp(info.creation);
+
+try {
+        pp = await client.profilePictureUrl(chat, 'image');
+      } catch {
+        pp = 'https://files.catbox.moe/duv8ac.jpg';
+      }
+
+await client.sendMessage(m.chat, { image: { url: pp }, 
+          caption: _Name_ : *${info.subject}*\n\n_ID_ : *${info.id}*\n\n_Group owner_ : ${'@'+info.owner.split('@')[0]} || 'No Creator'\n\n_Group created_ : *${ts.day}, ${ts.date} ${ts.month} ${ts.year}, ${ts.time}*\n\n_Participants_ : *${info.size}*\n_Members_ : *${info.participants.filter((p) => p.admin == null).length}*\n\n_Admins_ : *${Number(info.participants.length - info.participants.filter((p) => p.admin == null).length)}*\n\n_Who can send message_ : *${info.announce == true ? 'Admins' : 'Everyone'}*\n\n_Who can edit group info_ : *${info.restrict == true ? 'Admins' : 'Everyone'}*\n\n_Who can add participants_ : *${info.memberAddMode == true ? 'Everyone' : 'Admins'}*
+        }, {quoted: m })
+}
+	 break;			  
 		      
 case "autoread": {
 	if(!Owner) throw NotOwner;
